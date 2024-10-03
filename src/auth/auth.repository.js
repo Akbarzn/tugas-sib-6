9c568ba47126ef3aa09393f0d4f9f526fd6628dd
akbarzn <akbarzn13@gmail.com>
2024-10-04 02:05:24 +0700

    Tugas Individu SIB ke 6

---
+++ src/auth/auth.repository.js
@@ -0,0 +1,24 @@
+const prisma = require('../db')
+
+const createUser = async (userData)=>{
+    try{
+        const newUser = await prisma.user.create({ data:userData })
+        return newUser
+    } catch(error){
+        throw new Error('failed to create new user')
+    }
+}
+
+const findUsernameByUser = async (username)=>{
+    try{
+        return prisma.user.findUnique({where:{ username }})
+    }catch(error){
+        throw new Error('Login Failed')
+    }
+}
+module.exports = {
+    createUser,
+    findUsernameByUser
+}
+
+
